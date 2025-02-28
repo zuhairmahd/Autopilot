5151721ddeae5be08877b59f111bcc16361f33e4
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-02-28 01:08:22 -0500

    more fixes

--- GetHash.cmd
+++ GetHash.cmd
@@ -1,3 +1,2 @@
 @echo off
-pwsh\pwsh.exe -ExecutionPolicy Bypass -File Register-Device.ps1 -GetDeviceHash
--Check %*
+pwsh\pwsh.exe -ExecutionPolicy Bypass -File Register-Device.ps1 -GetDeviceHash %*
