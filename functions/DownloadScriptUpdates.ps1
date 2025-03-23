238389446c3c08e1bb5191e4fb8d234fe09bbebd
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-03-22 20:35:19 -0400

    Refactor DownloadScriptUpdates function to streamline script and command download logic

--- functions/DownloadScriptUpdates.ps1
+++ functions/DownloadScriptUpdates.ps1
@@ -15,7 +15,6 @@ function DownloadScriptUpdates()
     $success = $false
     Write-Verbose "Received ScriptRoot: $ScriptRoot"
     Write-Verbose "Received $($manifest.functions.count) functions, $($manifest.scripts.count) scripts, and $($manifest.cmds.count) cmds from the remote manifest." 
-    exit 0
     foreach ($type in $manifest.PSObject.Properties)
     {
         Write-Host "Processing $($type.Value.count) $($type.Name)"
@@ -26,51 +25,27 @@ function DownloadScriptUpdates()
             {
                 functions
                 {
-                    Write-Verbose "This file' has the method $($item.method)"
-                    if ($item.method -ne 'none')
-                    {
-                        $extension = 'ps1'
-                        $fullFunctionName = "$($item.Name).$extension"
-                        $localPath = Join-Path -Path $ScriptRoot -ChildPath "functions\$fullFunctionName"
-                        Write-Host "Downloading function $fullFunctionName from $($scriptURI)/$fullFunctionName to $localPath"
-                        # Invoke-WebRequest -Uri "$scriptURI/$fullFunctionName" -OutFile $localPath -UseBasicParsing -method Get -passThru -ErrorAction Stop
-                    }
-                    else
-                    {
-                        Write-Host "Function $($item.name).ps1 is up to date. Skipping download."
-                    }
+                    $extension = 'ps1'
+                    $fullFunctionName = "$($item.Name).$extension"
+                    $localPath = Join-Path -Path $ScriptRoot -ChildPath "functions\$fullFunctionName"
+                    Write-Host "Downloading function $fullFunctionName from $($scriptURI)/$fullFunctionName to $localPath"
+                    # Invoke-WebRequest -Uri "$scriptURI/$fullFunctionName" -OutFile $localPath -UseBasicParsing -method Get -passThru -ErrorAction Stop
                 }
                 scripts
                 {
-                    Write-Verbose "This file's has the method $($item.method)"
-                    if ($item.method -ne 'none')
-                    {
-                        $extension = 'ps1'
-                        $fullScriptName = "$($item.Name).$extension"
-                        $localPath = Join-Path -Path $ScriptRoot -ChildPath $fullScriptName
-                        Write-Host "Downloading script $fullScriptName from $($scriptURI)/$fullScriptName to $localPath"
-                        # Invoke-WebRequest -Uri "$scriptURI/$fullScriptName" -OutFile $localPath -UseBasicParsing -method Get -passThru -ErrorAction Stop
-                    }
-                    else
-                    {
-                        Write-Host "Script $($item.name).ps1 is up to date. Skipping download."
-                    }
+                    $extension = 'ps1'
+                    $fullScriptName = "$($item.Name).$extension"
+                    $localPath = Join-Path -Path $ScriptRoot -ChildPath $fullScriptName
+                    Write-Host "Downloading script $fullScriptName from $($scriptURI)/$fullScriptName to $localPath"
+                    # Invoke-WebRequest -Uri "$scriptURI/$fullScriptName" -OutFile $localPath -UseBasicParsing -method Get -passThru -ErrorAction Stop
                 }
                 cmds
                 {
-                    Write-Verbose "This file's has the method $($item.method)"
-                    if ($item.method -ne 'none')
-                    {
-                        $extension = 'cmd'
-                        $fullCmdName = "$($item.Name).$extension"
-                        $localPath = Join-Path -Path $ScriptRoot -ChildPath $fullCmdName
-                        Write-Host "Downloading cmd $fullCmdName from $($scriptURI)/$fullCmdName to $localPath"
-                        # Invoke-WebRequest -Uri "$scriptURI/$fullCmdName" -OutFile $localPath -UseBasicParsing -method Get -passThru -ErrorAction Stop
-                    }
-                    else
-                    {
-                        Write-Host "Cmd $($item.name).cmd is up to date. Skipping download."
-                    }
+                    $extension = 'cmd'
+                    $fullCmdName = "$($item.Name).$extension"
+                    $localPath = Join-Path -Path $ScriptRoot -ChildPath $fullCmdName
+                    Write-Host "Downloading cmd $fullCmdName from $($scriptURI)/$fullCmdName to $localPath"
+                    # Invoke-WebRequest -Uri "$scriptURI/$fullCmdName" -OutFile $localPath -UseBasicParsing -method Get -passThru -ErrorAction Stop
                 }
             }
         }
