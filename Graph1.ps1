b87ed958b70e435e261ceef8104f83d46fd4dafd
Zuhair Mahmoud <mahmoudz@gao.gov>
2025-04-08 18:41:16 -0400

    more code fixes

--- Graph1.ps1
+++
@@ -1,5 +0,0 @@
-
-[CmdletBinding()]
-param (
-)
-
--- test.ps1
+++ test.ps1
@@ -32,6 +32,97 @@ $accessToken = GetGraphAccessToken -configFile $configFile
 
 # $global:enrollmentState = VerifyEnrollmentStatus -serialNumber $serialNumber -accessToken $accessToken
 # Write-Host "Enrollment State: $($global:enrollmentState |ConvertTo-Json)" -ForegroundColor Green
-$global:devices = CallGraphAPI -AccessToken $accessToken -Uri $deviceManagementUri
+$global:devices = CallGraphAPI -AccessToken $accessToken -Uri $deviceManagementUri -Filter "startswith(OperatingSystem,'Windows')" -Method GET
 $global:imported = CallGraphAPI -AccessToken $accessToken -Uri $importedAutopilotDeviceURI -Method GET
 $global:autopilot = CallGraphAPI -AccessToken $accessToken -Uri $autoPilotDeviceURI -Method GET
+$global:matchingDevices = @()
+foreach ($global:autopilotDevice in $global:autopilot.value)
+{
+    Write-Host "Checking autopilot device with serial number: $($global:autopilotDevice.serialNumber) and deviceId: $($global:autopilotDevice.id)" -ForegroundColor Green
+    $id = $global:autopilotDevice.id
+    $productKey = $global:autopilotDevice.productKey
+    $azureActiveDirectoryDeviceId = $global:autopilotDevice.azureActiveDirectoryDeviceId
+    $azureAdDeviceId = $global:autopilotDevice.azureAdDeviceId
+    $managedDeviceId = $global:autopilotDevice.managedDeviceId
+    for ($i = 0; $i -lt $global:devices.value.Count; $i++)
+    {
+        $device = $global:devices.value[$i]
+        Write-Host "Checking device with serial number: $($device.serialNumber)"
+        foreach ($property in $device.PSObject.Properties)
+        {
+            if (($property.Name -like '*Id') -and ($null -ne $property.value) -and ($property.value -contains $azureAdDeviceId -or $property.value -contains $azureActiveDirectoryDeviceId -or $property.value -contains $managedDeviceId -or $property.value -contains $productKey -or $property.value -contains $id))
+            {
+                if ($property.value -contains $azureAdDeviceId)
+                {
+                    Write-Host "The property with name $($property.Name) contains the AzureAdDeviceId: $azureAdDeviceId" -ForegroundColor Yellow
+                    Write-Host "The Property tested: AzureAdDeviceId"
+                    Write-Host "The Property tested value: $azureAdDeviceId"
+                    Write-Host "The Matching Property name: $($property.Name)"
+                    Write-Host "The Matching Property value: $($property.value)"
+                    $global:matchingDevices += @{
+                        'PropertyTested'        = 'AzureAdDeviceId'
+                        'PropertyTestedValue'   = $azureAdDeviceId
+                        'MatchingPropertyName'  = $property.Name
+                        'MatchingPropertyValue' = $property.value
+                    }
+                }
+                if ($property.value -contains $azureActiveDirectoryDeviceId)
+                {
+                    Write-Host "The property with name $($property.Name) contains the AzureActiveDirectoryDeviceId: $azureActiveDirectoryDeviceId" -ForegroundColor Yellow
+                    Write-Host "The Property tested: AzureActiveDirectoryDeviceId"
+                    Write-Host "The Property tested value: $azureActiveDirectoryDeviceId"
+                    Write-Host "The Matching Property name: $($property.Name)"
+                    Write-Host "The Matching Property value: $($property.value)"
+                    $global:matchingDevices += @{
+                        'PropertyTested'        = 'AzureActiveDirectoryDeviceId'
+                        'PropertyTestedValue'   = $azureActiveDirectoryDeviceId
+                        'MatchingPropertyName'  = $property.Name
+                        'MatchingPropertyValue' = $property.value
+                    }
+                }
+                if ($property.value -contains $managedDeviceId)
+                {
+                    Write-Host "The property with name $($property.Name) contains the ManagedDeviceId: $managedDeviceId" -ForegroundColor Yellow
+                    Write-Host "The Property tested: ManagedDeviceId"
+                    Write-Host "The Property tested value: $managedDeviceId"
+                    Write-Host "The Matching Property name: $($property.Name)"
+                    Write-Host "The Matching Property value: $($property.value)"
+                    $global:matchingDevices += @{
+                        'PropertyTested'        = 'ManagedDeviceId'
+                        'PropertyTestedValue'   = $managedDeviceId
+                        'MatchingPropertyName'  = $property.Name
+                        'MatchingPropertyValue' = $property.value
+                    }
+                } 
+                if ($property.value -contains $productKey)
+                {
+                    Write-Host "The property with name $($property.Name) contains the ProductKey: $productKey" -ForegroundColor Yellow
+                    Write-Host "The Property tested: ProductKey"
+                    Write-Host "The Property tested value: $productKey"
+                    Write-Host "The Matching Property name: $($property.Name)"
+                    Write-Host "The Matching Property value: $($property.value)"
+                    $global:matchingDevices += @{
+                        'PropertyTested'        = 'ProductKey'
+                        'PropertyTestedValue'   = $productKey
+                        'MatchingPropertyName'  = $property.Name
+                        'MatchingPropertyValue' = $property.value
+                    }
+                }
+                if ($property.value -contains $id)
+                {
+                    Write-Host "The property with name $($property.Name) contains the Id: $id" -ForegroundColor Yellow
+                    Write-Host "The Property tested: Id"
+                    Write-Host "The Property tested value: $id"
+                    Write-Host "The Matching Property name: $($property.Name)"
+                    Write-Host "The Matching Property value: $($property.value)"
+                    $global:matchingDevices += @{
+                        'PropertyTested'        = 'Id'
+                        'PropertyTestedValue'   = $id
+                        'MatchingPropertyName'  = $property.Name
+                        'MatchingPropertyValue' = $property.value
+                    }
+                }
+            }
+        }
+    }
+}
