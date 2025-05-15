8e50d6d8383b6c72d36efefdb0dce4a63ca9cd02
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-05-15 01:04:17 -0400

    Enhance logging in Build.ps1 by replacing Write-Host with Write-Verbose for better traceability. Cleaned up function name processing logic and ensured consistent output formatting.

--- Build.ps1
+++ Build.ps1
@@ -13,18 +13,15 @@ Write-Host "Found $($filesList.Count) files in the current directory and subdire
 
 foreach ($file in $filesList)
 {
-    Write-Host "Processing file $($file.name)"
+    Write-Verbose "[$scriptName] Processing file $($file.name)"
     #Search the file for a string that starts with "function" and save the string either to the end of the line or to () in a variable.
-    $functionName = Select-String -Path $file.FullName -Pattern $FunctionPattern | ForEach-Object { $_.Matches.Groups[1].Value 
-        Write-Host $($_.Matches.Groups[1].Value)""
-    }
+    $functionName = Select-String -Path $file.FullName -Pattern $FunctionPattern | ForEach-Object { $_.Matches.Groups[1].Value }
     if ($functionName)
     {
-        # Write-Host "Found function $functionName in file $($file.name)"
         #Check if the function name is already in the list.
         if ($functionName -notin $functionsList)
         {
-            Write-Host "Adding function $functionName to the list."
+            Write-Verbose "[$scriptName] Adding function $functionName to the list."
             #Add the function name to the list.
             $functionsList += $functionName
         }
@@ -34,3 +31,11 @@ Write-Host "Found $($functionNames.count) functions"
 
 $functionsInFile = @()
 $functionsInFile = Select-String -Path $FileName -Pattern $FunctionPattern | ForEach-Object { $_.Matches.Groups[1].Value }
+$functionsInFile = $functionsInFile | Sort-Object -Unique
+Write-Host "Found $($functionsInFile.count) functions in file $($FileName)"
+#Print the list of functions in the file with a list format.
+Write-Host "Functions in file $($FileName):"
+foreach ($function in $functionsInFile)
+{
+    Write-Host " - $function"
+}
