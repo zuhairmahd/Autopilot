7560931162d045355ddbf7b6a45b68317bcedc04
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-05-16 07:59:05 -0400

    More work on the build function

--- Build.ps1
+++ Build.ps1
@@ -1,41 +1,67 @@
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory = $true)]
-    [string]$FileName
+    # [Parameter(Mandatory = $true)]
+    # [string]$FileName
 )
 
-#Get the name of the script.
-$scriptName = $MyInvocation.MyCommand.Name
-$FunctionPattern = "^function\s+([A-Za-z0-9\-]+)" 
-$filesList = Get-ChildItem -Path "$pwd\*.ps1" -Recurse
-Write-Verbose "[$scriptName] - Found $($filesList.Count) files in the current directory and subdirectories."
-Write-Host "Found $($filesList.Count) files in the current directory and subdirectories."
 
-foreach ($file in $filesList)
+# $scriptName = $MyInvocation.MyCommand.Name
+
+function BuildFunctionTable()
 {
-    Write-Verbose "[$scriptName] Processing file $($file.name)"
-    #Search the file for a string that starts with "function" and save the string either to the end of the line or to () in a variable.
-    $functionName = Select-String -Path $file.FullName -Pattern $FunctionPattern | ForEach-Object { $_.Matches.Groups[1].Value }
-    if ($functionName)
+    [CmdletBinding()]
+    param([string[]]$Path)
+    $functionName = $MyInvocation.MyCommand.Name
+    $FunctionPattern = "^function\s+([A-Za-z0-9\-]+)" 
+    $filesList = Get-ChildItem -Path "$Path\functions\*.ps1"
+    # Create a hashtable for quick function-to-file lookups
+    $functionToFile = @{}
+    Write-Verbose "[$functionName] - Found $($filesList.Count) files in the current directory and subdirectories."
+    Write-Host "Found $($filesList.Count) files in the function directory ."
+    foreach ($file in $filesList)
     {
-        #Check if the function name is already in the list.
-        if ($functionName -notin $functionsList)
+        Write-Verbose "[$functionName] Processing file $($file.name)"
+        $functionNames = @(Select-String -Path $file.FullName -Pattern $FunctionPattern | ForEach-Object { $_.Matches.Groups[1].Value })
+        Write-Verbose "[$functionName] Found $($functionNames.count) functions in file $($file.name)"
+        if ($functionNames)
         {
-            Write-Verbose "[$scriptName] Adding function $functionName to the list."
-            #Add the function name to the list.
-            $functionsList += $functionName
+            Write-Verbose "[$functionName] Adding $($functionNames.count) functions to the list."
+            $functionList = @()
+            foreach ($functionName in $functionNames)
+            {
+                Write-Verbose "[$functionName] Adding function $functionName to the list."
+                $functionList += $functionName
+                # Add to our lookup hashtable - function name is the key, file name is the value
+                $functionToFile[$functionName] = $file.Name
+            }
+        }
+        else
+        {
+            Write-Verbose "[$functionName] No functions found in file $($file.name)"
         }
     }
+    return $functionToFile
 }
-Write-Host "Found $($functionNames.count) functions"
 
-$functionsInFile = @()
-$functionsInFile = Select-String -Path $FileName -Pattern $FunctionPattern | ForEach-Object { $_.Matches.Groups[1].Value }
-$functionsInFile = $functionsInFile | Sort-Object -Unique
-Write-Host "Found $($functionsInFile.count) functions in file $($FileName)"
-#Print the list of functions in the file with a list format.
-Write-Host "Functions in file $($FileName):"
-foreach ($function in $functionsInFile)
+
+function FindFunctionFile
 {
-    Write-Host " - $function"
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$FunctionNameToFind
+    )
+
+    $functionName = $MyInvocation.MyCommand.Name
+    Write-Verbose "[$functionName] Looking for function: $FunctionNameToFind"
+    if ($global:functionMap.ContainsKey($FunctionNameToFind))
+    {
+        return $functionMap[$FunctionNameToFind]
+    }
+    else
+    {
+        Write-Warning "Function '$FunctionName' not found in any files."
+        return $null
+    }
 }
+
