e9316d6e7233986b97411c998572647226349a42
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-05-25 01:04:12 -0400

    Refactor code structure for improved readability and maintainability

--- functions/CommonMenuFunctions.ps1
+++ functions/CommonMenuFunctions.ps1
@@ -6,51 +6,6 @@ function DisplayNumericMenu()
         [string[]]$choices,
         [string]$banner = "Please press the number of your choice and press enter.",
         [string]$Prompt = "Please select an option",
-        $errorMessage = "Invalid selection. Please try again."
-    )
-    #Print a verbose message with received    
-    Write-Verbose "[$functionName] Received parameters: $($choices | Out-String)"
-    Write-Verbose "[$functionName] Prompt: $Prompt"
-    Write-Verbose "[$functionName] ErrorMessage: $errorMessage"
-
-    $functionName = $MyInvocation.MyCommand.Name
-    # Display the menu options
-    Write-Host $banner -ForegroundColor Green
-    for ($i = 0; $i -lt $choices.Count; $i++)
-    {
-        Write-Host "$($i + 1). $($choices[$i])"
-    }
-    Write-Host "0. Exit"
-    # Prompt for user input
-    $selection = Read-Host -Prompt $Prompt
-    # Validate the selection
-    while ($selection -notmatch '^\d+$' -or [int]$selection -lt 0 -or [int]$selection -gt $choices.Count)
-    {
-        Write-Host $errorMessage -ForegroundColor Red
-        #beep
-        [console]::beep(1000, 500)
-        $selection = Read-Host -Prompt $Prompt
-    }
-    if ($selection -ne 0)
-    {
-        # Return the selected choice
-        return $choices[[int]$selection - 1]
-    }
-    else
-    {
-        Write-Verbose "[$functionName] Exiting script."
-        return $selection
-    }
-}
-
-function DisplayNumericMenu()
-{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string[]]$choices,
-        [string]$banner = "Please press the number of your choice and press enter.",
-        [string]$Prompt = "Please select an option",
         $errorMessage = "Invalid selection. Please try again.",
         [switch]$RequireEnter
     )
@@ -199,7 +154,6 @@ function AddMenuItem()
         [switch]$ReturnsValue
     )
     $functionName = $MyInvocation.MyCommand.Name
-    
     if ($Action -and $Submenu)
     {
         throw "A menu item cannot have both an Action and a Submenu."
@@ -227,7 +181,6 @@ function AddMenuItem()
         Write-Verbose "[$functionName] Type: submenu"
     }
     Write-Verbose "[$functionName] returns value: $ReturnsValue"
-
     $Menu.Items += $item
     return $Menu
 }
