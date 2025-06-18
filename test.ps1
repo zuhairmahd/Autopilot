5ebd442e06d937482b2ced9643e725ba0f7bc588
Zuhair Mahmoud <mahmoudz@gao.gov>
2025-06-17 20:37:20 -0400

    Enhance user retrieval and display functionality with substring search and improved error handling

--- test.ps1
+++ test.ps1
@@ -1,9 +1,94 @@
 [CmdletBinding()]
 param
 (
+    [Parameter(Mandatory = $true, Position = 0)]
+    $userName,
+    $repo = 'Github', # Options: Github, gitlab
+    $release = 'auto',
     $configFile = "$pwd\.secrets\config.json",
     $outputFile = "$pwd\deviceMemory-export.csv"
 )
+
+#region Load parameters from the configuration file if it exists
+$scriptName = $MyInvocation.MyCommand.Name
+$initFile = "$pwd\settings.json"
+$domain = Get-Content -Path $configFile -Raw -Force -ErrorAction Stop | ConvertFrom-Json | Select-Object -ExpandProperty domain
+Write-Verbose "[$scriptName] Domain: $domain"
+if (Test-Path -Path $InitFile)
+{
+    Write-Host " Loading configuration values from $(Split-Path -Path $initFile -Leaf)"
+    $global:globalSettings = @{}
+    $global:localSettings = @{}
+    $globalConfigData = Get-Content -Path $InitFile -Raw -Force | ConvertFrom-Json | Select-Object -ExpandProperty 'globalSettings'
+    Write-Verbose "[$scriptName] Reading global settings..."
+    Write-Verbose "[$scriptName] Found $($globalConfigData.PSObject.Properties.Name.count) configurations."
+    foreach ($key in $globalConfigData.PSObject.Properties.Name)
+    {
+        Write-Verbose "[$scriptName] Checking if $($key) was provided on the command line."
+        if ($PSBoundParameters.ContainsKey($key) -eq $false -and $null -ne $globalConfigData.$key)
+        {
+            Write-Verbose "[$scriptName] Read parameter $key from the configuration file as $($globalConfigData.$key)"
+            Write-Verbose "[$scriptName] Setting $key to $($globalConfigData.$key)"
+            if ($globalConfigData.$key -in ('true', 'false'))
+            {
+                Write-Verbose "[$scriptName] Converting $key to boolean."
+                $keyBooleanValue = [bool]::Parse($globalConfigData.$key)
+                $globalSettings.add($key, $keyBooleanValue)
+                Write-Verbose "[$scriptName] Setting the value of $key to the boolean value ($keybooleanValue)."
+                # Set-Variable -Name $key -Value $keyBooleanValue
+            }
+            else
+            {
+                Write-Verbose "[$scriptName] Setting the value of $key to the string value ($($globalConfigData.$key))."
+                # Set-Variable -Name $key -Value $globalConfigData.$key
+                $globalSettings.add($key, $globalConfigData.$key)
+            }
+        }
+        else
+        {
+            Write-Verbose "[$scriptName] Got parameter $key from the commandline as $($PSBoundParameters[$key])"
+            #add it to the global settings hashtable.
+            $globalSettings.add($key, $PSBoundParameters[$key])
+        }
+    }
+    $localConfigData = (Get-Content -Path $InitFile -Raw -Force | ConvertFrom-Json | Select-Object -ExpandProperty "domains").$domain
+    Write-Verbose "[$scriptName] Reading local settings for domain $domain..."
+    Write-Verbose "[$scriptName] Found $($localConfigData.PSObject.Properties.Name.count) configurations."
+    foreach ($key in $localConfigData.PSObject.Properties.Name)
+    {
+        Write-Verbose "[$scriptName] Checking if $($key) was provided on the command line."
+        if ($PSBoundParameters.ContainsKey($key) -eq $false -and $null -ne $localConfigData.$key)
+        {
+            Write-Verbose "[$scriptName] Read parameter $key from the configuration file as $($localConfigData.$key)"
+            Write-Verbose "[$scriptName] Setting $key to $($localConfigData.$key)"
+            if ($localConfigData.$key -in ('true', 'false'))
+            {
+                Write-Verbose "[$scriptName] Converting $key to boolean."
+                $keyBooleanValue = [bool]::Parse($localConfigData.$key)
+                $localSettings.add($key, $keyBooleanValue)
+                Write-Verbose "[$scriptName] Setting the value of $key to the boolean value ($keybooleanValue)."
+                # Set-Variable -Name $key -Value $keyBooleanValue
+            }
+            else
+            {
+                Write-Verbose "[$scriptName] Setting the value of $key to the string value ($($localConfigData.$key))."
+                # Set-Variable -Name $key -Value $localConfigData.$key
+                $localSettings.add($key, $localConfigData.$key)
+            }
+        }
+        else
+        {
+            Write-Verbose "[$scriptName] Read parameter $key from the commandline as $($PSBoundParameters[$key])"
+            #add it to the local settings hashtable.
+            $localSettings.add($key, $PSBoundParameters[$key])
+        }
+    }   
+}
+else
+{
+    Write-Host "Configuration file $initFile not found. Using default values."
+}
+#endregion Load parameters from the configuration file if it exists
 
 #region import functions.
 $functionsFolder = "$PWD\functions"
@@ -26,7 +111,7 @@ else
 
 #region variables
 # $logfile = "mylog.log"
-# $domain = Get-Content -Path $configFile -Raw -Force -ErrorAction Stop | ConvertFrom-Json | Select-Object -ExpandProperty domain
+$settings = MergeSettings -localSettings $localSettings -globalSettings $globalSettings -ConflictResolution 'Local'
 # $serialNumber = '0F3CFP724223KV'
 # $serialNumber = 'BTSB25000BCR'
 # $serialNumber = '5R3SBZ3'
@@ -46,7 +131,7 @@ else
 # $deviceConfigurationUri = "deviceManagement/deviceConfigurations"
 # $autopilotCsv = [System.Collections.ArrayList]@()
 # $importedCsv = [System.Collections.ArrayList]@()
-# $accessToken = GetGraphAccessToken -configFile $configFile -deligated -scope $scopes -AuthType 'PublicAuthFlow' 
+$accessToken = GetGraphAccessToken -configFile $configFile -deligated -scope $scopes -AuthType 'PublicAuthFlow' 
 # $accessToken = GetGraphAccessToken -configFile $configFile
 # $autopilotDevices = CallGraphApi -ResourcePath $autoPilotDeviceURI -accessToken $accessToken -extraParameters $autopilotExtraParameters -consistencyLevel -verbose
 # $importedDevices = CallGraphApi -ResourcePath $importedAutopilotDeviceURI -accessToken $accessToken -consistencyLevel -extraParameters $importedAutopilotDeviceExtraParameters -verbose
@@ -59,47 +144,18 @@ else
 # }
 #endregion variables
 
-$invocation = $MyInvocation
-#print all the content of $myinvocation.
-Write-Host "Script name: $($invocation.MyCommand.Name)"
-Write-Host "Script path: $($invocation.MyCommand.Path)"
-Write-Host "Script line number: $($invocation.ScriptLineNumber)"
-Write-Host "Script command type: $($invocation.MyCommand.CommandType)"
-Write-Host "Script arguments: $($invocation.MyCommand.Parameters | Out-String)"
-Write-Host "$($invocation.MyCommand.Name) started at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" -ForegroundColor Green
-Write-Host "Command type: $($invocation.MyCommand.CommandType)"
-Write-Host "Command name: $($invocation.MyCommand.Name)"
-Write-Host "Command path: $($invocation.MyCommand.Path)"
-Write-Host "Command line number: $($invocation.ScriptLineNumber)"
-Write-Host "Command arguments: $($invocation.MyCommand.Parameters | Out-String)"    
 
-if ($invocation.MyCommand.CommandType -eq "ExternalScript")
+
+$global:users = GetEntraUser -accessToken $accessToken -UserName $userName -FindSimilar
+Write-Host "Found $($global:users[0].value.Count) users matching '$userName'"
+if ($global:users[1] -eq $true)
 {
-    Write-Host "Running as an external script."
-    $ScriptPath = Split-Path -Parent -Path $invocation.MyCommand.Definition
-    Write-Host "Script path: $ScriptPath"
+    $userNameSelection = DisplayUserList -UserList $global:users[0].value -userName $userName
+    Write-Host "Selected user: $userNameSelection"
 }
-else
+else 
 {
-    Write-Host "Running as a script block."
-    $ScriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
-    Write-Host "Script path: $ScriptPath"
-    if (!$ScriptPath)
-    {
-        Write-Host "Script path is not set. Defaulting to current directory."
-        $ScriptPath = "."
-    }
+    Write-Host "Found $($global:users[0].value.Count) users matching '$userName'"
 }
 
-Write-Host "Script name: $($invocation.MyCommand.Name)"
-Write-Host "Script path: $($invocation.MyCommand.Path)"
-Write-Host "Script line number: $($invocation.ScriptLineNumber)"
-Write-Host "Script command type: $($invocation.MyCommand.CommandType)"
-Write-Host "Script arguments: $($invocation.MyCommand.Parameters | Out-String)"
-Write-Host "$($invocation.MyCommand.Name) started at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" -ForegroundColor Green
-Write-Host "Command type: $($invocation.MyCommand.CommandType)"
-Write-Host "Command name: $($invocation.MyCommand.Name)"
-Write-Host "Command path: $($invocation.MyCommand.Path)"
-Write-Host "Command line number: $($invocation.ScriptLineNumber)"
-Write-Host "Command arguments: $($invocation.MyCommand.Parameters | Out-String)"    
 
