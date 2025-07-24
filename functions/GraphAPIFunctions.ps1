dcc397164ca02d7cc5055a5331c68ec17dbe33a4
Zuhair Mahmoud <mahmoudz@gao.gov>
2025-07-24 15:15:45 -0400

    Fixed refresh token issue when running in interactive or private mode

--- functions/GraphAPIFunctions.ps1
+++ functions/GraphAPIFunctions.ps1
@@ -1210,13 +1210,17 @@ function Test-RefreshTokenValidity()
         $AuthType
     )
     $functionName = $MyInvocation.MyCommand.Name
-    Write-Verbose "[$functionName] Testing refresh token validity..."
+    #write verbose and log the function name and parameters
+    Write-Verbose "[$functionName] Testing refresh token validity with parameters:clientId=$clientId, tenantId=$tenantId, domain=$domain, AuthType=$AuthType"
+    Write-Log -LogFile $LogFile -Module "$functionName" -Message "Testing refresh token validity with parameters: refreshToken=$refreshToken, clientId=$clientId, tenantId=$tenantId, scopes=$scopes, domain=$domain, AuthType=$AuthType"
     try
     {
         # Ensure scopes are properly formatted for the token refresh
         $scopesFormatted = $scopes
         if ($scopes -and -not $scopes.Contains("https://graph.microsoft.com/"))
         {
+            Write-Verbose "[$functionName] Scopes provided, formatting them for token refresh"
+            Write-Log -LogFile $LogFile -Module "$functionName" -Message "Scopes provided, formatting them for token refresh" -LogLevel "Verbose"
             $scopesArray = $scopes.Split(' ', [StringSplitOptions]::RemoveEmptyEntries)
             $formattedScopesArray = @()
             foreach ($scope in $scopesArray)
@@ -1238,12 +1242,19 @@ function Test-RefreshTokenValidity()
             grant_type    = 'refresh_token'
             scope         = $scopesFormatted
         }
-        if ($AuthType -eq 'PublicAuthFlow')
+        if ($auth.AuthType -eq 'PublicAuthFlow')
         {
+            Write-Verbose "[$functionName] Public authentication flow detected, client secret will not be included in the request"
+            Write-Log -LogFile $LogFile -Module "$functionName" -Message "Public authentication flow detected, client secret will not be included in the request" -LogLevel "Verbose"
+        }
+        else
+        {
+            Write-Verbose "[$functionName] Non-public authentication flow detected, client secret will be included in the request"
+            Write-Log -LogFile $LogFile -Module "$functionName" -Message "Non-public authentication flow detected, client secret will be included in the request" -LogLevel "Verbose"
             $refreshTokenRequestBody.Add('client_secret', $clientSecret)
         }
         $refreshTokenEndpoint = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
-        Write-Verbose "[$functionName] Attempting to validate refresh token by getting a new access token..."
+        Write-Verbose "[$functionName] Attempting to validate refresh token by getting a new access token.."
         $tokenResponse = Invoke-RestMethod -Method Post -Uri $refreshTokenEndpoint -ContentType "application/x-www-form-urlencoded" -Body $refreshTokenRequestBody
         Write-Verbose "[$functionName] Refresh token is valid. Successfully obtained a new access token."
         return $true, $tokenResponse
