65784a9425ad8445c0858a367baaa71177b1f491
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-08-05 13:06:34 -0400

    Fixed extra space in menu which caused the menu inclusion logic to break

--- main.ps1
+++ main.ps1
@@ -1582,7 +1582,7 @@ $mainMenu = AddMenuItem -Menu $mainMenu -Name "Give a device to a user" -Action 
         }
     }
 }
-$mainMenu = AddMenuItem -Menu $mainMenu -Name "Check device status " -Submenu $CheckMenu
+$mainMenu = AddMenuItem -Menu $mainMenu -Name "Check device status" -Submenu $CheckMenu
 $mainMenu = AddMenuItem -menu $mainMenu -Name "Autopilot menu" -Submenu $autopilotMenu
 $mainMenu = AddMenuItem -menu $mainMenu -Name "Change application settings" -Submenu $settingsMenu
 $mainMenu = AddMenuItem -menu $mainMenu -Name "Check for script updates" -Action {
