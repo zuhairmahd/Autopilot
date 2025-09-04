847ade9d2f2d091e70bbe4d786f625c341d5c512
Zuhair Mahmoud <mahmoudz@gao.gov>
2025-09-04 15:10:45 -0400

    Fixed an issue with the expirey of the accesstoken on disk

--- functions/graphFunctions/Get-NormalizedExpiryTime.ps1
+++ functions/graphFunctions/Get-NormalizedExpiryTime.ps1
@@ -5,9 +5,11 @@ function Get-NormalizedExpiryTime()
         [object]$accessTokenObject
     )
     $functionName = $MyInvocation.MyCommand.Name
+    Write-Verbose "[$functionName] Starting function execution"
     if (-not $accessTokenObject.AbsoluteExpiryTime)
     {
         Write-Verbose "[$functionName] No AbsoluteExpiryTime found in token object"
+        write-log -logFile $logFile -moduleName $moduleName -logLevel Warning -message "No AbsoluteExpiryTime found in token object"
         return [datetime]::MinValue
     }
     
@@ -16,8 +18,9 @@ function Get-NormalizedExpiryTime()
         if ($accessTokenObject.AbsoluteExpiryTime -is [string])
         {
             Write-Verbose "[$functionName] Converting string expiry time to datetime"
+            write-log -logFile $logFile -moduleName $moduleName -logLevel Verbose -message "Converting string expiry time to datetime"
             $parsedTime = [datetime]::Parse($accessTokenObject.AbsoluteExpiryTime).ToLocalTime()
-            
+            write-log -logFile $logFile -moduleName $moduleName -logLevel Verbose -message "Parsed expiry time: $parsedTime"
             # Handle timezone differences
             if ($parsedTime -lt $accessTokenObject.AbsoluteExpiryTime)
             {
@@ -25,6 +28,11 @@ function Get-NormalizedExpiryTime()
                 return $accessTokenObject.AbsoluteExpiryTime
             }
             return $parsedTime
+        }
+        elseif ($accessTokenObject.AbsoluteExpiryTime.kind -eq 'Utc')
+        {
+            Write-Verbose "[$functionName] Converting UTC expiry time to local time"
+            return $accessTokenObject.AbsoluteExpiryTime.ToLocalTime()
         }
         else
         {
@@ -35,6 +43,7 @@ function Get-NormalizedExpiryTime()
     catch
     {
         Write-Warning "[$functionName] Failed to parse expiry time: $_"
+        write-log -logFile $logFile -moduleName $moduleName -logLevel Error -message "Failed to parse expiry time: $_"
         return [datetime]::MinValue
     }
 }
