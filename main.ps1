6de3aea0fd2476ea4c8bf59a809ff854c387579e
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-09-12 22:56:54 -0400

    Added strings.psd1 to the list of files to be downloaded with an update

--- main.ps1
+++ main.ps1
@@ -645,7 +645,7 @@ if ($updateAvailable.success -eq $true -and $updateAvailable.version -gt $versio
         Write-Host "Automatic updates are enabled." -ForegroundColor Green
         Write-Host "The script will now attempt to update itself." -ForegroundColor Yellow
         Write-Log -LogFile $LogFile -Module "$scriptName" -Message "Automatic updates are enabled. The script will now attempt to update itself." -LogLevel "Information"
-        $updateResult = GetUpdates -executableFileName "$scriptPath\$scriptName" -updateURL $updateURL -noConfirmation
+        $updateResult = GetUpdates -executableFileName "$scriptPath\$scriptName" -updateURL $updateURL -SupportingFiles @($menuFile, $stringsFile) -noConfirmation
         Write-Log -LogFile $LogFile -Module "$scriptName" -Message "Update result: $updateResult" -LogLevel "Information"
         switch ($updateResult)
         {
@@ -1805,7 +1805,7 @@ $mainMenu = AddMenuItem -menu $mainMenu -Name "Autopilot menu" -Submenu $autopil
 $mainMenu = AddMenuItem -menu $mainMenu -Name "Change application settings" -Submenu $settingsMenu
 $mainMenu = AddMenuItem -menu $mainMenu -Name "Check for script updates" -Action {
     Write-Host "Checking for script updates..."
-    $updateResult = GetUpdates -executableFileName "$scriptPath\$scriptName" -updateURL $updateURL -SupportingFiles $menuFile
+    $updateResult = GetUpdates -executableFileName "$scriptPath\$scriptName" -updateURL $updateURL -SupportingFiles @($menuFile, $stringsFile) 
     Write-Verbose "[$scriptName] Update result: $updateResult"
     switch ($updateResult)
     {
