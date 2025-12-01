e91fb50b7c030fbea903c73fbfb2ff9fd07922aa
Zuhair Mahmoud <zuhair@accesstojobs.com>
2025-11-30 22:38:16 -0500

    Enhance menu functions: Add Description parameter to AddMenuItem and update ShowMenu for improved item display.  Fixed datastructures to properly display and handle return values

--- functions/menuFunctions/ShowMenu.ps1
+++ functions/menuFunctions/ShowMenu.ps1
@@ -220,18 +220,18 @@ function ShowMenu()
         $includeItem = Test-MenuItemIncluded -MenuItemName $item.Name -Menus $menusToPass -CurrentMenu $Menu
         if ($includeItem)
         {
-            Write-Verbose "[$functionName] Adding item: $($item.Name)"
+            Write-Verbose "[$functionName] Adding item: $($item.Name) with description: $($item.Description)            "
             # Build a hashtable with name and description for DisplayNumericMenu
             $choiceItem = @{
                 name        = $item.Name
-                description = if ($item.description) { $item.description } else { "" }
+                description = if ($item.Description) { $item.Description } else { "" }
             }
             $choices += $choiceItem
             $menuItems += $item
         }
         else
         {
-            Write-Verbose "[$functionName] Excluding item: $($item.Name)"
+            Write-Verbose "[$functionName] Excluding item: $($item.Name) with description: $($item.Description)            "
             Write-Log -LogFile $LogFile -Module $functionName -Message "Excluding menu item from display: $($item.Name)" -LogLevel "Information"
         }
     }
@@ -244,12 +244,12 @@ function ShowMenu()
     if ($Global:MenuHistory.Count -gt 1)
     {
         Write-Verbose "[$functionName] Adding 'Back' option since depth is $($Global:MenuHistory.Count)"
-        $choices += @{ name = "Back"; description = "" }
+        $choices += @{ name = "Back"; description = "Return to the previous menu" }
     }
     if ($Global:MenuHistory.Count -gt 2)
     {
         Write-Verbose "[$functionName] Adding 'Main Menu' option since depth is $($Global:MenuHistory.Count)"
-        $choices += @{ name = "Main Menu"; description = "" }
+        $choices += @{ name = "Main Menu"; description = "Return to the main menu" }
     }
     #endregion
 
